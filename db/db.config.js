1649605983a42a8fd2f20b24c0ebec1b68568943
Rei Ikonomi <94234882+onomdev@users.noreply.github.com>
2022-02-24 19:17:45 +0100

    Update db.config.js

--- db/db.config.js
+++ db/db.config.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 const mysql = require("mysql");
 
 const conn = mysql.createPool({
-  host: "linux18.host.al",
-  user: "algridal_shopcon1",
-  password: "Ae36du~2",
-  database: "algridal_shopcon1",
+  host: "",
+  user: "",
+  password: "",
+  database: "",
   connectionLimit: 50,
 });
 
